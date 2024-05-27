22af67f2199b4518737299a39b00d444963af3a7
halcyon-past <titanssuperior@gmail.com>
2024-05-27 14:38:44 +0530

    Added Question for twosum

--- 02 Two Pointer/01 TwoSum.py
+++ 02 Two Pointer/01 TwoSum.py
@@ -1,3 +1,15 @@
+"""
+Given an array and a target value, return the indices of two numbers that add up to the target value.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+"""
+
+
+
 def twosum(nums,target):
     l = 0
     r = len(nums)-1
