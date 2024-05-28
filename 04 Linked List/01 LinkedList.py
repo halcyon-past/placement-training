b1a22740517c6ea3ecf1c4e543266bc692c535cb
halcyon-past <titanssuperior@gmail.com>
2024-05-28 17:28:56 +0530

    Linked List

---
+++ 04 Linked List/01 LinkedList.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data, end=" ")
+            cur_node = cur_node.next
+        print()
+
+if __name__ == "__main__":
+    llist = LinkedList()
+    llist.append(1)
+    llist.append(2)
+    llist.append(3)
+    llist.print_list()
