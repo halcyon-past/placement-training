b8fc3fd530348e047dde5ba3f3a1ac9ef9909043
halcyon-past <titanssuperior@gmail.com>
2024-06-05 13:12:58 +0530

    Linked List Practice

--- 04 Linked List/01 LinkedList.py
+++ 04 Linked List/01 LinkedList.py
@@ -6,6 +6,11 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+    
+    def prepend(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
 
     def append(self, data):
         new_node = Node(data)
@@ -16,6 +21,14 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
+    
+    def length(self):
+        count = 0
+        cur_node = self.head
+        while cur_node:
+            count += 1
+            cur_node = cur_node.next
+        return count
 
     def print_list(self):
         cur_node = self.head
@@ -29,4 +42,5 @@ if __name__ == "__main__":
     llist.append(1)
     llist.append(2)
     llist.append(3)
+    llist.prepend(0)
     llist.print_list()
