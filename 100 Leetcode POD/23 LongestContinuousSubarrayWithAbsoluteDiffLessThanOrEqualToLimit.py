8fb787aa0cfebbead2d5ea5dfc7a2b8b5367a4ca
halcyon-past <titanssuperior@gmail.com>
2024-06-23 16:57:22 +0530

    Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

---
+++ 100 Leetcode POD/23 LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.py
@@ -0,0 +1,84 @@
+"""
+1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
+
+ 
+
+Example 1:
+
+Input: nums = [8,2,4,7], limit = 4
+Output: 2 
+Explanation: All subarrays are: 
+[8] with maximum absolute diff |8-8| = 0 <= 4.
+[8,2] with maximum absolute diff |8-2| = 6 > 4. 
+[8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+[2] with maximum absolute diff |2-2| = 0 <= 4.
+[2,4] with maximum absolute diff |2-4| = 2 <= 4.
+[2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+[4] with maximum absolute diff |4-4| = 0 <= 4.
+[4,7] with maximum absolute diff |4-7| = 3 <= 4.
+[7] with maximum absolute diff |7-7| = 0 <= 4. 
+Therefore, the size of the longest subarray is 2.
+Example 2:
+
+Input: nums = [10,1,2,4,7,2], limit = 5
+Output: 4 
+Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+Example 3:
+
+Input: nums = [4,2,2,2,4,4,2,2], limit = 0
+Output: 3
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+0 <= limit <= 109
+"""
+
+from typing import List
+
+from collections import deque
+
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        min_deque = deque()
+        max_deque = deque()
+        left = 0
+        max_len = 0
+        
+        for right in range(len(nums)):
+            while min_deque and nums[right] < nums[min_deque[-1]]:
+                min_deque.pop()
+            while max_deque and nums[right] > nums[max_deque[-1]]:
+                max_deque.pop()
+                
+            min_deque.append(right)
+            max_deque.append(right)
+            
+            while nums[max_deque[0]] - nums[min_deque[0]] > limit:
+                left += 1
+                if min_deque[0] < left:
+                    min_deque.popleft()
+                if max_deque[0] < left:
+                    max_deque.popleft()
+                    
+            max_len = max(max_len, right - left + 1)
+            
+        return max_len
+    
+# Test Cases
+
+test = Solution()
+assert test.longestSubarray([8,2,4,7], 4) == 2
+assert test.longestSubarray([10,1,2,4,7,2], 5) == 4
+assert test.longestSubarray([4,2,2,2,4,4,2,2], 0) == 3
+
+print("All passed")
