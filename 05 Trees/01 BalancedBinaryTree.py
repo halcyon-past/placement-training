1d92b5cc3adf22698bd056d3388ea33c7d6b1af7
halcyon-past <titanssuperior@gmail.com>
2024-07-11 02:26:01 +0530

    Balanced Binary Tree

---
+++ 05 Trees/01 BalancedBinaryTree.py
@@ -0,0 +1,72 @@
+"""
+
+Code
+
+
+Testcase
+Testcase
+Test Result
+110. Balanced Binary Tree
+Solved
+Easy
+Topics
+Companies
+Given a binary tree, determine if it is 
+height-balanced
+.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+
+Input: root = []
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-104 <= Node.val <= 104
+"""
+
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class TreeNode:
+    def __init__(self,val=0,left=None,right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(root):
+            if not root: return [True,0]
+
+            left,right = dfs(root.left),dfs(root.right)
+
+            balanced = (left[0] and right[0]) and (abs(left[1]-right[1])<=1)
+
+            return [balanced,1+max(left[1],right[1])]
+        
+        return dfs(root)[0]
+
+# Test Cases
+
