1b2b254f7db5580b6d3e216519c56afc1a3b7b22
halcyon-past <titanssuperior@gmail.com>
2024-07-12 00:42:14 +0530

    Updated

--- 05 Trees/02 InvertBinaryTree.py
+++ 05 Trees/02 InvertBinaryTree.py
@@ -36,6 +36,15 @@ The number of nodes in the tree is in the range [0, 100].
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self,val=0,left=None,right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if root == None:
