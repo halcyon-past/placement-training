d853324cef14eb208b6ea253d9e76fa6ede554cf
halcyon-past <titanssuperior@gmail.com>
2024-08-08 23:03:26 +0530

    Binary Tree Zigzag Level Order Traversal

---
+++ 102 Leetcode Top Interview 150/10 Binary Tree BFS/03 BinaryTreeZigZagLevelOrderTraversal.py
@@ -0,0 +1,79 @@
+"""
+103. Binary Tree Zigzag Level Order Traversal
+Solved
+Medium
+Topics
+Companies
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+"""
+
+from typing import List, Optional
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+        if not root:
+            return []
+        
+        res = []
+        left_to_right = True
+        q = deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    if left_to_right:
+                        level.append(node.val)
+                    else:
+                        level.insert(0,node.val)
+
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+
+            res.append(level)
+            left_to_right=not(left_to_right)
+        
+        return res
