98cde10d8f0b2e99f5bd1533bc6333512e0e70c0
halcyon-past <titanssuperior@gmail.com>
2024-08-25 14:12:42 +0530

    Binary Tree Postorder Traversal

---
+++ 100 Leetcode POD/86 BinaryTreePostOrderTraversal.py
@@ -0,0 +1,76 @@
+"""
+145. Binary Tree Postorder Traversal
+Solved
+Easy
+Topics
+Companies
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+from typing import List, Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        l = []
+        def helper(root):
+            if not root:
+                return l
+            helper(root.left)
+            helper(root.right)
+            l.append(root.val)
+        helper(root)
+        return l
+
+class SolutionIterative:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return res[::-1]
