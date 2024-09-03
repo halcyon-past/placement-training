294af13cd7fdea8ffd3b1736d5c9aaf86b90b752
halcyon-past <titanssuperior@gmail.com>
2024-09-03 13:54:56 +0530

    Sum of Digits of String After Convert

---
+++ 100 Leetcode POD/95 SumOfDigitsOfStringAfterConvert.py
@@ -0,0 +1,79 @@
+"""
+1945. Sum of Digits of String After Convert
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given a string s consisting of lowercase English letters, and an integer k.
+
+First, convert s into an integer by replacing each letter with its position in the alphabet (i.e., replace 'a' with 1, 'b' with 2, ..., 'z' with 26). Then, transform the integer by replacing it with the sum of its digits. Repeat the transform operation k times in total.
+
+For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+
+Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+Transform #2: 17 ➝ 1 + 7 ➝ 8
+Return the resulting integer after performing the operations described above.
+
+ 
+
+Example 1:
+
+Input: s = "iiii", k = 1
+Output: 36
+Explanation: The operations are as follows:
+- Convert: "iiii" ➝ "(9)(9)(9)(9)" ➝ "9999" ➝ 9999
+- Transform #1: 9999 ➝ 9 + 9 + 9 + 9 ➝ 36
+Thus the resulting integer is 36.
+Example 2:
+
+Input: s = "leetcode", k = 2
+Output: 6
+Explanation: The operations are as follows:
+- Convert: "leetcode" ➝ "(12)(5)(5)(20)(3)(15)(4)(5)" ➝ "12552031545" ➝ 12552031545
+- Transform #1: 12552031545 ➝ 1 + 2 + 5 + 5 + 2 + 0 + 3 + 1 + 5 + 4 + 5 ➝ 33
+- Transform #2: 33 ➝ 3 + 3 ➝ 6
+Thus the resulting integer is 6.
+Example 3:
+
+Input: s = "zbax", k = 2
+Output: 8
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+1 <= k <= 10
+s consists of lowercase English letters.
+"""
+
+class Solution:
+    def single(self, string: str,k: int) -> int:
+        count = string
+        while (k!=0):
+            num = 0
+            for i in count:
+                num += int(i)
+            count = str(num)
+            k-=1
+        return int(count)
+
+
+    def getLucky(self, s: str, k: int) -> int:
+        string = ""
+
+        for i in s:
+            string += str(ord(i)-96)
+        
+        return self.single(string,k)
+
+# Test Cases
+
+test = Solution()
+
+assert test.getLucky("iiii", 1) == 36
+assert test.getLucky("leetcode", 2) == 6
+assert test.getLucky("zbax", 2) == 8
+
+print("All test cases pass")
