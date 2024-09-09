d6cbdc77f0846c03aeaa50c22e22eaa6309c55ff
halcyon-past <titanssuperior@gmail.com>
2024-09-09 13:50:01 +0530

    Spiral Matrix IV

--- 100 Leetcode POD/101 SpiralMatrixIV.py
+++ 100 Leetcode POD/101 SpiralMatrixIV.py
@@ -38,3 +38,58 @@ Constraints:
 The number of nodes in the list is in the range [1, m * n].
 0 <= Node.val <= 1000
 """
+
+from typing import List, Optional
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        matrix = [[-1] * n for _ in range(m)]
+        
+        topRow, bottomRow = 0, m - 1
+        leftColumn, rightColumn = 0, n - 1
+        
+        while head:
+            # Fill top row
+            for col in range(leftColumn, rightColumn + 1):
+                if not head:
+                    break
+                matrix[topRow][col] = head.val
+                head = head.next
+            topRow += 1
+            
+            # Fill right column
+            for row in range(topRow, bottomRow + 1):
+                if not head:
+                    break
+                matrix[row][rightColumn] = head.val
+                head = head.next
+            rightColumn -= 1
+            
+            # Fill bottom row
+            for col in range(rightColumn, leftColumn - 1, -1):
+                if not head:
+                    break
+                matrix[bottomRow][col] = head.val
+                head = head.next
+            bottomRow -= 1
+            
+            # Fill left column
+            for row in range(bottomRow, topRow - 1, -1):
+                if not head:
+                    break
+                matrix[row][leftColumn] = head.val
+                head = head.next
+            leftColumn += 1
+        
+        return matrix
