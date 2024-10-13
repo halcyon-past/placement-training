df630136ff1855ecf29828988bf20fb54e3aab49
halcyon-past <titanssuperior@gmail.com>
2024-10-13 12:17:44 +0530

    Smallest Range Covering Elements from K Lists

---
+++ 100 Leetcode POD/135 SmallestRangeCoveringElementsFromKLists.py
@@ -0,0 +1,57 @@
+"""
+632. Smallest Range Covering Elements from K Lists
+Solved
+Hard
+Topics
+Companies
+You have k lists of sorted integers in non-decreasing order. Find the smallest range that includes at least one number from each of the k lists.
+
+We define the range [a, b] is smaller than range [c, d] if b - a < d - c or a < c if b - a == d - c.
+
+ 
+
+Example 1:
+
+Input: nums = [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]
+Output: [20,24]
+Explanation: 
+List 1: [4, 10, 15, 24,26], 24 is in range [20,24].
+List 2: [0, 9, 12, 20], 20 is in range [20,24].
+List 3: [5, 18, 22, 30], 22 is in range [20,24].
+Example 2:
+
+Input: nums = [[1,2,3],[1,2,3],[1,2,3]]
+Output: [1,1]
+ 
+
+Constraints:
+
+nums.length == k
+1 <= k <= 3500
+1 <= nums[i].length <= 50
+-105 <= nums[i][j] <= 105
+nums[i] is sorted in non-decreasing order.
+"""
+
+from typing import List
+import heapq
+
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        heap = []
+        cur_max = float('-inf')
+        for i in range(len(nums)):
+            heapq.heappush(heap, (nums[i][0], i, 0)) 
+            cur_max = max(cur_max, nums[i][0])
+        small = [float('-inf'), float('inf')]
+        while heap:
+            cur_min, list_idx, i = heapq.heappop(heap)
+            if (cur_max - cur_min < small[1] - small[0]) or ((cur_max - cur_min == small[1] - small[0]) and cur_min < small[0]):
+                small = [cur_min, cur_max]
+            if i + 1 < len(nums[list_idx]):
+                nxt = nums[list_idx][i + 1]
+                heapq.heappush(heap, (nxt, list_idx, i+1))
+                cur_max = max(cur_max, nxt)
+            else:
+                break
+        return small
