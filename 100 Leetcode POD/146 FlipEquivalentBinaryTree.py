2b45bad05d3598d40dd34b61312e40e8ed1f24e8
halcyon-past <titanssuperior@gmail.com>
2024-10-24 17:20:57 +0530

    Flip Equivalent Binary Trees

---
+++ 100 Leetcode POD/146 FlipEquivalentBinaryTree.py
@@ -0,0 +1,71 @@
+"""
+951. Flip Equivalent Binary Trees
+Solved
+Medium
+Topics
+Companies
+For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.
+
+ 
+
+Example 1:
+
+Flipped Trees Diagram
+Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+Output: true
+Explanation: We flipped at nodes with values 1, 3, and 5.
+Example 2:
+
+Input: root1 = [], root2 = []
+Output: true
+Example 3:
+
+Input: root1 = [], root2 = [1]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in each tree is in the range [0, 100].
+Each tree will have unique node values in the range [0, 99].
+"""
+
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        
+        def checker(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            return ((checker(node1.left, node2.left) or checker(node1.left, node2.right)) and
+                    (checker(node1.right, node2.right) or checker(node1.right, node2.left)))
+        
+        return checker(root1, root2)
+
+# Test cases
+
+test = Solution()
+assert test.flipEquiv([1,2,3,4,5,6,None,None,None,7,8], [1,3,2,None,6,4,5,None,None,None,None,8,7]) == True
+assert test.flipEquiv([], []) == True
+assert test.flipEquiv([], [1]) == False
