18d8bdd29eb68faf6b1016fca9a0989852a43f0a
halcyon-past <titanssuperior@gmail.com>
2025-02-03 13:16:42 +0530

    Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ 100 Leetcode POD/0249 LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.py
@@ -0,0 +1,95 @@
+"""
+3105. Longest Strictly Increasing or Strictly Decreasing Subarray
+Solved
+Easy
+Topics
+Companies
+You are given an array of integers nums. Return the length of the longest 
+subarray
+ of nums which is either 
+strictly increasing
+ or 
+strictly decreasing
+.
+
+ 
+
+Example 1:
+
+Input: nums = [1,4,3,3,2]
+
+Output: 2
+
+Explanation:
+
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+
+Hence, we return 2.
+
+Example 2:
+
+Input: nums = [3,3,3,3]
+
+Output: 1
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+
+Hence, we return 1.
+
+Example 3:
+
+Input: nums = [3,2,1]
+
+Output: 3
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [2], and [1].
+
+The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+
+Hence, we return 3.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+"""
+
+from typing import List
+
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        n, ans, inc, dec=len(nums), 0, 1, 1
+        if n==1: return 1
+        for i in range(1, n):
+            if nums[i]>nums[i-1]:
+                inc+=1
+                dec=1
+            elif nums[i]<nums[i-1]:
+                inc=1
+                dec+=1
+            else:
+                inc=dec=1
+            ans=max(ans, dec, inc)
+        return ans
+
+
+#  Test Cases
+
+test = Solution()
+
+assert test.longestMonotonicSubarray([1,4,3,3,2]) == 2
+assert test.longestMonotonicSubarray([3,3,3,3]) == 1
+assert test.longestMonotonicSubarray([3,2,1]) == 3
+
+print("All passed")
+        
