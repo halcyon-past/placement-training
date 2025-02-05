327e3855730ec95eed54274c733f3e512379ef0e
halcyon-past <titanssuperior@gmail.com>
2025-02-05 23:02:34 +0530

    Check if One String Swap Can Make Strings Equal

---
+++ 100 Leetcode POD/0251 CheckIfOneStringSwapCanMakeStringsEqual.py
@@ -0,0 +1,51 @@
+"""
+1790. Check if One String Swap Can Make Strings Equal
+Easy
+Topics
+Companies
+Hint
+You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s1 = "bank", s2 = "kanb"
+Output: true
+Explanation: For example, swap the first character with the last character of s2 to make "bank".
+Example 2:
+
+Input: s1 = "attack", s2 = "defend"
+Output: false
+Explanation: It is impossible to make them equal with one string swap.
+Example 3:
+
+Input: s1 = "kelb", s2 = "kelb"
+Output: true
+Explanation: The two strings are already equal, so no string swap operation is required.
+ 
+
+Constraints:
+
+1 <= s1.length, s2.length <= 100
+s1.length == s2.length
+s1 and s2 consist of only lowercase English letters.
+"""
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        i=-1
+        j=-1
+        cnt=0
+        for k in range(0, len(s1)):
+            if s1[k]!=s2[k]:
+                cnt+=1
+                if i==-1: i=k
+                elif j==-1: j=k
+        
+        if cnt==0: return True
+        elif cnt==2 and s1[i]==s2[j] and s1[j]==s2[i]: return True
+
+        return False
