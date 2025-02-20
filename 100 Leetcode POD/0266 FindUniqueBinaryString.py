b18a871395674da2c9ee35d5efd2bafcb4b31c00
halcyon-past <titanssuperior@gmail.com>
2025-02-20 19:24:41 +0530

    Find Unique Binary String

---
+++ 100 Leetcode POD/0266 FindUniqueBinaryString.py
@@ -0,0 +1,60 @@
+"""
+1980. Find Unique Binary String
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+
+ 
+
+Example 1:
+
+Input: nums = ["01","10"]
+Output: "11"
+Explanation: "11" does not appear in nums. "00" would also be correct.
+Example 2:
+
+Input: nums = ["00","01"]
+Output: "11"
+Explanation: "11" does not appear in nums. "10" would also be correct.
+Example 3:
+
+Input: nums = ["111","011","001"]
+Output: "101"
+Explanation: "101" does not appear in nums. "000", "010", "100", and "110" would also be correct.
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 16
+nums[i].length == n
+nums[i] is either '0' or '1'.
+All the strings of nums are unique.
+"""
+
+from typing import List
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        seen = set(nums)
+        n = len(nums)
+
+        for i in range(1 << n):  
+            binary = format(i, '0' + str(n) + 'b')
+            if binary not in seen:
+                return binary
+        return ""
+
+
+# Test Cases
+
+test = Solution()
+
+assert test.findDifferentBinaryString(["01","10"]) == "11" or test.findDifferentBinaryString(["01","10"]) == "00"
+assert test.findDifferentBinaryString(["00","01"]) == "11" or test.findDifferentBinaryString(["00","01"]) == "10"
+assert test.findDifferentBinaryString(["111","011","001"]) == "101" or test.findDifferentBinaryString(["111","011","001"]) == "000" or test.findDifferentBinaryString(["111","011","001"]) == "010" or test.findDifferentBinaryString(["111","011","001"]) == "100" or test.findDifferentBinaryString(["111","011","001"]) == "110"
+
+print("All passed")
