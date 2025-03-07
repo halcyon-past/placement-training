a1df2b2de6ecd0db7703d5a51130f13744542e1a
halcyon-past <titanssuperior@gmail.com>
2025-03-07 22:57:50 +0530

    Closest Prime Numbers in Range

---
+++ 100 Leetcode POD/0281 ClosestPrimeNumberInRange.py
@@ -0,0 +1,60 @@
+"""
+2523. Closest Prime Numbers in Range
+Solved
+Medium
+Topics
+Companies
+Hint
+Given two positive integers left and right, find the two integers num1 and num2 such that:
+
+left <= num1 < num2 <= right .
+Both num1 and num2 are prime numbers.
+num2 - num1 is the minimum amongst all other pairs satisfying the above conditions.
+Return the positive integer array ans = [num1, num2]. If there are multiple pairs satisfying these conditions, return the one with the smallest num1 value. If no such numbers exist, return [-1, -1].
+
+ 
+
+Example 1:
+
+Input: left = 10, right = 19
+Output: [11,13]
+Explanation: The prime numbers between 10 and 19 are 11, 13, 17, and 19.
+The closest gap between any pair is 2, which can be achieved by [11,13] or [17,19].
+Since 11 is smaller than 17, we return the first pair.
+Example 2:
+
+Input: left = 4, right = 6
+Output: [-1,-1]
+Explanation: There exists only one prime number in the given range, so the conditions cannot be satisfied.
+ 
+
+Constraints:
+
+1 <= left <= right <= 106
+ 
+"""
+
+class Solution:
+    def __init__(self):
+        self.N = 10**6 + 2
+        self.prime = [1] * self.N
+        self.seive()
+
+    def seive(self):
+        self.prime[0] = self.prime[1] = 0
+        for i in range(2, int(self.N**0.5) + 1):
+            if self.prime[i]:
+                for j in range(i * i, self.N, i):
+                    self.prime[j] = 0
+
+    def closestPrimes(self, left: int, right: int):
+        prev, diff = -1, float('inf')
+        ans = [-1, -1]
+
+        for i in range(left, right + 1):
+            if self.prime[i]:
+                if prev != -1 and i - prev < diff:
+                    ans = [prev, i]
+                    diff = i - prev
+                prev = i
+        return ans
